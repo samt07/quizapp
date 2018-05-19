30a50e4b0196d985e42c5669c23c9eef2c1c2592
Sampath T <miscapps111@gmail.com>
2018-05-19 19:49:13 -0400

    test deploy to GH

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var airportJson = require('./public/data/airportsFull.json');
 var airportCodeName = [];
 
 if (!airportJson ==''){
-  
+
 
   for (var i=0 ; i < airportJson.length; i++) {
     airportCodeName.push(('"'+airportJson[i].iata + ' ' + airportJson[i].name+'"'));
@@ -24,22 +24,22 @@ if (!airportJson ==''){
 
   airportJson = JSON.stringify(airportJson);
   fs.writeFile('./public/data/airportsCopy.json', airportCodeName);
-    
+
   }
 */
 
 
-/*Deva
-const passport = require('passport'); Deva */
+//Deva
+
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.static('public'));
 app.use(express.static('node_modules/babel-standalone'));
-app.set('views', './views'); //this is set by ejs engine by default. 
+app.set('views', './views'); //this is set by ejs engine by default.
 //If we have a folder named 'views', we need not have to specify this.
 //But if we want to keep our view files some other location, then use this
 //to specify that path. Ejs take care of it otherwise.
-app.set('view engine', 'ejs'); //express built in property to specify html templating 
+app.set('view engine', 'ejs'); //express built in property to specify html templating
 //engine
 
 app.use(quizApp.session);
@@ -67,16 +67,15 @@ app.use(require('morgan')('combined', {
 
 app.use('/', quizApp.router);
 
-quizApp.ioServer(app).listen(app.get('port'), () => { 
+quizApp.ioServer(app).listen(app.get('port'), () => {
 	console.log('QuizApp Running on port :', app.get('port'));
 })
 
 //The above stmt was like below before we brought in ioServer.
-//app.listen(app.get('port'), () => { 
+//app.listen(app.get('port'), () => {
 
-//This is because, as we know, app is tied to express which has its own server implementation. That was why we were able to use app.listen directly. 
+//This is because, as we know, app is tied to express which has its own server implementation. That was why we were able to use app.listen directly.
 //Now to bring in socket, we need to first create an 'http'	 module server instance for the express 'app' and then bind that to a
-//socket.io server instance. Then use that socket.io server instance to listen to the app.get('port'). 
+//socket.io server instance. Then use that socket.io server instance to listen to the app.get('port').
 //chatCat.ioServer refers to the index.js file under app. We could see the above process happening there for ioServer. Also, for more clear
 //explanation, look at the lecture 59 last 5 minutes.
-	
